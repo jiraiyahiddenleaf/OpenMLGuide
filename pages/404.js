803495e60a59ed763fd2f4929ad4c7aeeaa43ab0
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2023-10-08 12:21:06 +0530

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div id="main">
+      <div class="fof">
+        <h1>Error 404</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+
+import 'components/404.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
